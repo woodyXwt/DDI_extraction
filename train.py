a7f3921a951e0a57f0335d2e79f4ade96f5a2853
woodyXwt <54299037+woodyXwt@users.noreply.github.com>
2020-06-24 18:55:31 +0800

    Update train.py

--- train.py
+++ train.py
@@ -98,7 +98,7 @@ assert emb_matrix.shape[1] == opt['emb_dim']
 # load data
 print("Loading data from {} with batch size {}...".format(opt['data_dir'], opt['batch_size']))
 train_batch = DataLoader(opt['data_dir'] + '/train.json', opt['batch_size'], opt, vocab, evaluation=False)
-dev_batch = DataLoader(opt['data_dir'] + '/dev.json', opt['batch_size'], opt, vocab, evaluation=True)
+dev_batch = DataLoader(opt['data_dir'] + '/test.json', opt['batch_size'], opt, vocab, evaluation=True)
 
 model_id = opt['id'] if len(opt['id']) > 1 else '0' + opt['id']
 model_save_dir = opt['save_dir'] + '/' + model_id
